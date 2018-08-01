a6f3c93314d080c64ccc61596dac602b47f655a3
root <brad@4real.live>
2018-08-01 23:46:45 +1000

    Added NYC and CANADA

--- js/config.js
+++ js/config.js
@@ -1,16 +1,17 @@
 const apiInterface = 'https://api.turtlenode.io/'
 const nodes = [
-//  { name: 'US-East-1', hostname: 'node-1.nyc.turtlenode.io', port: 12001, region: 'US East' },
+  { name: 'USA-1', hostname: 'node-1.nyc.athenanode.io', port: 12001, region: 'US East' },
 //  { name: 'US-East-2', hostname: 'node-2.nyc.turtlenode.io', port: 12001, region: 'US East' },
 //  { name: 'US-East-3', hostname: 'node-3.nyc.turtlenode.io', port: 11898, region: 'US East' },
-  { name: 'US-West-1', hostname: 'node-1.flo.athenanode.io', port: 12001, region: 'US West' },
+  { name: 'USA-2', hostname: 'node-1.flo.athenanode.io', port: 12001, region: 'US West' },
 //  { name: 'US-West-2', hostname: 'node-2.sfo.turtlenode.io', port: 11898, region: 'US West' },
 //  { name: 'US-West-3', hostname: 'node-3.sfo.turtlenode.io', port: 11898, region: 'US West' },
-  { name: 'EU-AMS-1', hostname: 'node-1.ams.athenanode.io', port: 12001, region: 'Europe' }, 
-  { name: 'EU-LON-1', hostname: 'node-1.lon.athenanode.io', port: 12001, region: 'Europe' },
-  { name: 'EU-FRA-1', hostname: 'node-1.fra.athenanode.io', port: 12001, region: 'Europe' },
-  { name: 'AS-SIN-1', hostname: 'node-1.sgp.athenanode.io', port: 12001, region: 'Asia' },
+  { name: 'AMS-1', hostname: 'node-1.ams.athenanode.io', port: 12001, region: 'Europe' }, 
+  { name: 'LON-1', hostname: 'node-1.lon.athenanode.io', port: 12001, region: 'Europe' },
+  { name: 'GER-1', hostname: 'node-1.fra.athenanode.io', port: 12001, region: 'Europe' },
+  { name: 'SIN-1', hostname: 'node-1.sgp.athenanode.io', port: 12001, region: 'Asia' },
 //  { name: 'AS-SIN-2', hostname: 'node-2.sin.turtlenode.io', port: 11898, region: 'Asia' },
 //  { name: 'AS-SIN-3', hostname: 'node-3.sin.turtlenode.io', port: 11898, region: 'Asia' },
+  { name: 'TOR-1', hostname: 'node-1.tor.athenanode.io', port: 12001, region: 'Canada' },
 //  { name: 'SEED', hostname: 'seed.athenanode.io', port: 12001, region: 'US West' }
 ]
